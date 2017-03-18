7b1c5964cffeaf4741ec8e2b007eda4f4bb581a1
vinh <nhquocvinh@gmail.com>
2017-03-18 13:13:02 +0700

    this is the third commit message ahihi

--- public/scripts/bubble_sort/index.js
+++ public/scripts/bubble_sort/index.js
@@ -1,3 +1,24 @@
-function bubble_sort() {
+function bubble_sort(a) {
+    var swapped;
+    var count = 0;
+    do {
+        swapped = false;
+        for (var i = 0; i < a.length - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                var temp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+                swapped = true;
+                count++;
+            }
+        }
+    } while (swapped);
 
+    return count;
 }
+
+var a = [34, 203, 3, 746, 200, 984, 198, 764, 9];
+
+var r = bubble_sort(a);
+console.log(a);
+console.log(r);
